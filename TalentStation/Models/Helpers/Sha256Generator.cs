f1787b0fc7217ea692803dee2437ab4f30ed593c
MattShah <mzawiski97@gmail.com>
2021-02-23 19:49:40 +0100

    Added password hasher and encryptor

---
+++ TalentStation/Models/Helpers/Sha256Generator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalentStation.Models.Helpers
+{
+    public class Sha256Generator
+    {
+        public static string ComputeString(string text)
+        {
+            using (SHA256 mySHA256 = SHA256.Create())
+            {
+                byte[] sourceBytes = Encoding.UTF8.GetBytes(text);
+                byte[] hashBytes = mySHA256.ComputeHash(sourceBytes);
+                return BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+            }
+        }
+    }
+}
---
+++ TalentStation/Models/Helpers/StringEncrypter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TalentStation.Models.Helpers
+{
+    public class StringEncrypter
+    {
+        public static string Encrypt(string key, string text)
+        {
+            using Aes aes = Aes.Create();
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = new byte[16];
+            aes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+            using MemoryStream memoryStream = new MemoryStream();
+            using CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+            using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+            {
+                streamWriter.Write(text);
+            }
+
+            return Convert.ToBase64String(memoryStream.ToArray());
+        }
+
+        public static string Decrypt(string key, string encryptedText)
+        {
+            using Aes aes = Aes.Create();
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = new byte[16];
+            aes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            using MemoryStream memoryStream = new MemoryStream(Convert.FromBase64String(encryptedText));
+            using CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            using StreamReader streamReader = new StreamReader(cryptoStream);
+
+            return streamReader.ReadToEnd();
+        }
+    }
+}
